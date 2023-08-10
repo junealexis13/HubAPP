ff1ef530fe5326480ff5e51358e06f3586e8e331
junealexis13 <junealexis.santos13@gmail.com>
2023-08-10 13:29:53 +0800

    initial addition

--- main.py
+++ main.py
@@ -1,3 +1,20 @@
 import pandas as pd
 import glob, os
+import streamlit as st
 
+from st_pages import Page, show_pages, add_page_title
+
+# Optional -- adds the title and icon to the current page
+add_page_title('HubAPP')
+
+# Specify what pages should be shown in the sidebar, and what their titles and icons
+# should be
+show_pages(
+    [
+        Page("pages/home.py", "Home", "🏠"),
+        Page("pages/dtr.py", "DTR", ":books:"),
+    ]
+)
+
+
+st
---
+++ pages/dtr.py
@@ -0,0 +1,7 @@
+import pandas as pd
+import streamlit as st
+
+st.set_page_config('HubAPP - DTR',page_icon=':book:',layout='centered')
+st.title('Generate DTR')
+st.divider()
+
